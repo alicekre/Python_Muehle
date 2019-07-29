0bb50736cd0cea6bc7e0b3d77e40cefc637e94eb
alicekre <53276683+alicekre@users.noreply.github.com>
2019-07-29 14:53:03 +0200

    Add files via upload

---
+++ Spielfeld_zeichnen_2.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 27 22:35:42 2019
+
+@author: ask
+"""
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets, uic 
+
+Ui_MainWindow, WindowBaseClass = uic.loadUiType("Spielfeld_OF_2.ui")
+
+class MyDialog(WindowBaseClass, Ui_MainWindow):
+    def __init__(self, parent=None):
+        WindowBaseClass.__init__(self, parent)
+        Ui_MainWindow.__init__(self)
+        self.setupUi(self)
+        
+        self.resize(1100, 700)
+        self.update()
+        
+if __name__ == "__main__":    
+    # In Spyder kann nur eine Qt-Applikation laufen und sie werden nicht anschliessend geloescht
+    if QtCore.QCoreApplication.instance() is not None:
+        app = QtCore.QCoreApplication.instance()
+    else:
+        app = QtWidgets.QApplication(sys.argv)
+
+    dialog = MyDialog()
+    dialog.show()
+    sys.exit(app.exec_())
+    
