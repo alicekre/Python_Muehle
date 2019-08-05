674b22366a9850a73f2cf9e6baea951dce8be1ab
alicekre <alice.krehl@lherk.de>
2019-08-05 20:12:37 +0200

    playing field is opened

---
+++ main.py
@@ -0,0 +1,12 @@
+from playing_field import initUI
+
+
+class main():
+    initUI()
+
+    #dialog = MyDialog()
+    #dialog.
+### Zugriff aus Funktionen aus Klassen aus anderen Dateien/Skripten
+
+if __name__ == "__main__":
+    main()
