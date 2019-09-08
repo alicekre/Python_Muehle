c6b7c3fc6d044e7d1d304d1283eb96a8b6702211
kiralay <kira.layer@gmx.de>
2019-09-08 20:16:24 +0200

    Revert "COMMIT"
    This reverts commit b50d373d9fa51f5c4b7943548e4e00999c246f1f.
    
    revert commit#

--- open_window.py
+++ open_window.py
@@ -7,26 +7,29 @@ Created on Mon Aug  5 19:44:56 2019
 from PyQt5.QtWidgets import QWidget
 from PyQt5 import QtWidgets
 
+if True:                            #ersetzten: nur wenn Spiel fertig
+    class App(QWidget):
 
-class Window(QWidget):
-
-    def __init__(self):  #text anpassen
-        super().__init__()
-        self.left = 800
-        self.top = 400
-        self.width = 320
-        self.height = 200
+        def __init__(self,text):  #text anpassen
+            super().__init__()
+            self.title = 'End'
+            self.left = 800
+            self.top = 400
+            self.width = 320
+            self.height = 200
+            self.text=text
+            self.initUI()
+        
+        
+         
+           
+        def initUI(self):
+            label_new_window=QtWidgets.QLabel(self)
+            label_new_window.setText(self.text)
+            self.setWindowTitle(self.title)
+            self.setGeometry(self.left, self.top, self.width, self.height)
+                
+            self.show()
     
-    
-     
-       
-    def initUI(self, text, title):
-        label_new_window=QtWidgets.QLabel(self)
-        label_new_window.setText(text)
-        self.setWindowTitle(title)
-        self.setGeometry(self.left, self.top, self.width, self.height)
-            
-        self.show()
-
 
 
--- token_label.py
+++ token_label.py
@@ -7,9 +7,6 @@ class token_label(QLabel):
     def __init__(self, parent):
         super(QLabel, self).__init__(parent)
         self.show()
-        
-    def saveDialog(self, dialog):
-        self.dialog = dialog
 
     def mousePressEvent(self, event):
         print("Pressed")
@@ -37,5 +34,3 @@ class token_label(QLabel):
         self.clear()
         print("Moving")
         drag.exec_(Qt.MoveAction)
-       
-       
